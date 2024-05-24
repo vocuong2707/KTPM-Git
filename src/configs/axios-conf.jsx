edf3f02fe382b79914b24d63c4543368114ee7d2
vocuong2707 <82353211+vocuong2707@users.noreply.github.com>
2024-05-25 02:54:42 +0700

    Update axios-conf.jsx

--- src/configs/axios-conf.jsx
+++ src/configs/axios-conf.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://18.141.86.142:3001',
+  baseURL: '18.141.86.142:3001',
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
