323958314bb3a876f54609e5552785b7ba366dd5
vocuong2707 <cuongvo.26022020@gmail.com>
2024-05-25 11:09:48 +0700

    update

--- src/configs/axios-conf.jsx
+++ src/configs/axios-conf.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://18.141.86.142:3001',
+  baseURL: 'https://server-zelo.onrender.com',
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? "" : "http://18.141.86.142:3001";
+  process.env.NODE_ENV === "production" ? "" : "https://server-zelo.onrender.com";
 
 export const socket = io(URL);
