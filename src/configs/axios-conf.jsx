eb2405bcc29db1f283e7e03921114b78ba6c8051
vocuong2707 <cuongvo.26022020@gmail.com>
2024-05-25 11:19:29 +0700

    HelloHello

--- src/configs/axios-conf.jsx
+++ src/configs/axios-conf.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://server-zelo.onrender.com',
+  baseURL: 'https://server-zelo.onrender.com/',
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
